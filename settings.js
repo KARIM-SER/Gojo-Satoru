0344510d0f47e857ea44aebacc0658dc05ce8067
KARIM SER <99970448+KARIM-SER@users.noreply.github.com>
2022-06-07 20:15:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,78 +1,135 @@
 //═══════════════════════════════════════════════════════//
+
 //
+
 //                              NOTORI-X 𝓫𝔂 ī.am K҉A҉R҉I҉M҉ ҉s҉e҉r  
+
 //𝙰𝙳𝙾𝙿𝚃𝙴𝙳 𝙵𝚁𝙾𝙼  𝚂𝙲𝚁𝙸𝙿𝚃 𝙾𝙵 𝙲𝙷𝙴𝙴𝙼𝚂𝙱𝙾𝚃 𝚅2 𝙱𝚈 𝙳𝙶𝚇𝚎𝚘𝚗 
+
 //
+
 //════════════════════════════//
 
 const fs = require('fs')
+
 const chalk = require('chalk')
 
 //Api Website\\
+
 global.APIs = {
-	zenz: 'https://zenzapis.xyz',
-}
+
+	zenz: 'https://zenzapis.xyz',}
 
 //Api Key\\
+
 global.APIKeys = {
+
 	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+
 }
 
 //customize settings\\
+
 global.owner = ['918075927661']
+
 global.premium = ['918975927661']
+
 global.ownernomer = '918075927661'
+
 global.ownername = ' ī.am K҉A҉R҉I҉M҉ ҉s҉e҉r'
+
 global.botname = 'Notori-X V-1'
+
 global.footer = '©Team Notori-X.'
+
 global.ig = 'https://github.com/KARIM-SER'
+
 global.region = 'India, South India, Kerala'
+
 global.sc = 'https://github.com/KARIM-SER/Gojo-Satoru'
+
 global.myweb = 'https://instagram.com/karim_ser_?igshid=YmMyMTA2M2Y='
+
 global.packname = Notori-X 
 
-
-
-
-
 global.author = ' ī.am K҉A҉R҉I҉M҉ ҉s҉e҉r'
+
 global.sessionName = 'session'
+
 global.prefa = [''.']
+
 global.sp = '⭔'
+
 global.mess = {
+
     success: 'Done ✓',
+
     admin: 'This Feature Is Only For Admin!',
+
     botAdmin: 'Admin aak fundee 😒🤹🏻‍♂️!',
+
     owner: 'This Feature Is Only For Owner!',
+
     group: 'Feature Used Only For Groups!',
+
     private: 'Features Used Only For Private Chat!',
+
     bot: 'This Feature Is Only For Bot',
+
     wait: 'Wait Cheyy Mwonu....',
+
     error: 'Error! Maybe Api Key Is Expired🤔!',
+
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+
 }
+
     global.limitawal = {
+
     premium: "Infinity",
+
     free: 12,
+
     monayawal: 1000
+
 }
+
    global.rpg = {
+
    darahawal: 100,
+
    besiawal: 15,
+
    goldawal: 10,
+
    emeraldawal: 5,
+
    umpanawal: 5,
+
    potionawal: 1
+
 }
+
 global.thumb = fs.readFileSync('./GojoMedia/gojo.jpg')
+
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
 let file = require.resolve(__filename)
+
 fs.watchFile(file, () => {
+
 	fs.unwatchFile(file)
+
 	console.log(chalk.redBright(`Update'${__filename}'`))
+
 	delete require.cache[file]
+
 	require(file)
+
 })
